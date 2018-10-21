7dc5f980c30a86a40339bb52c906fbaa48e0cde2
Artem <ketzenkrad@gmail.com>
2018-10-22 01:36:51 +0300

    Урок девятый и десятый, правки и прочее.

--- Lession 9/HW00/js/script.js
+++ Lession 9/HW00/js/script.js
@@ -76,4 +76,32 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('timer', deadline);
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        moreTabs = document.querySelectorAll('.description-btn');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
+    for (let i = 0; i < moreTabs.length; i++) {
+        let buttonMore = moreTabs[i];
+
+        buttonMore.addEventListener('click', function(event) {
+            if (event.target && event.target.className == 'description-btn') {
+                event.target.classList.add('more-splash');
+                overlay.style.display = "block";
+                document.body.style.overflow = 'hidden';
+            }
+        });
+    }
 });
