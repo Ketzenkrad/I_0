2877e52df8b9f838afa3d33aaa4e8c590899a55a
Artem <ketzenkrad@gmail.com>
2018-10-30 21:17:49 +0300

    Правки, Webpack

--- Lession 15, webpack/HW00/src/parts/slider.js
+++ Lession 15, webpack/HW00/src/parts/slider.js
@@ -1,49 +1,55 @@
 function slider() {
     let slideIndex = 1,
-        slides = document.querySelectorAll('.slider-item'),
-        preview = document.querySelector('.prev'),
+        slides = document.getElementsByClassName('slider-item'),
+        prev = document.querySelector('.prev'),
         next = document.querySelector('.next'),
-        activeDot = document.querySelector('.slider-dots'),
-        allDots = document.querySelectorAll('.dot');
-show(slideIndex);
-    function show(s) {
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.getElementsByClassName('dot');
 
-        if (s > slideIndex.length) {
+    showSlides(slideIndex);
+
+    function showSlides(s) {
+        if (n > slides.length) {
             slideIndex = 1;
         }
-        if (s < 1) {
+        if (n < 1) {
             slideIndex = slides.length;
         }
 
-        slides.forEach((item) => item.style.display = 'none');
-        allDots.forEach((item) => item.classList.remove('dot-active'));
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = 'none';
+        }
+
+        for (let i = 0; i < dots.length; i++) {
+            dots[i].classList.remove('dot-active');
+        }
+
         slides[slideIndex - 1].style.display = 'block';
-        allDots[slideIndex - 1].classList.add('dot-active');
+        dots[slideIndex - 1].classList.add('dot-active');
     }
 
-    function plusSlide(s) {
-        show(slideIndex += s);
+    function plusSlides(s) {
+        showSlides(slideIndex += n);
     }
 
-    function current(s) {
-        show(slideIndex = s);
+    function currentSlide(s) {
+        showSlides(slideIndex = n);
     }
 
-    preview.addEventListener('click', function() {
-        plusSlide(-1);
+    prev.addEventListener('click', function(e) {
+        plusSlides(-1);
     });
-    next.addEventListener('click', function() {
-        plusSlide(1);
+    next.addEventListener('click', function(e) {
+        plusSlides(1);
     });
 
-    activeDot.addEventListener('click', function(event) {
-        for (let i = 0; i < allDots.length + 1; i++) {
-            if (event.target.classList.contains('dot') && event.target == allDots[counter - 1]) {
-                current(i);
+    dotsWrap.addEventListener('click', function(e) {
+        for (var i = 0; i < dots.length + 1; i++) {
+            if (e.target.classList.contains('dot') && e.target == dots[i - 1]) {
+                currentSlide(i);
             }
         }
     });
-    
 }
 
 module.exports = slider;
